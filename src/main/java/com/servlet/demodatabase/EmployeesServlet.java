9a2d753fd58abcfbe35f18678eb6da710cf57906
Aiman Anizan <71580787+AimanAnizan56@users.noreply.github.com>
2021-12-06 23:09:30 +0800

    Add comments

--- src/main/java/com/servlet/demodatabase/EmployeesServlet.java
+++ src/main/java/com/servlet/demodatabase/EmployeesServlet.java
@@ -17,13 +17,15 @@ public class EmployeesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        PrintWriter out = response.getWriter(); // get PrintWriter from HTTPServletResponse
+
         out.println("<html>");
         out.println("<head>");
-        out.println("<link href='style.css' rel='stylesheet'>");
+        out.println("<link href='style.css' rel='stylesheet'>"); // include CSS stylesheet
         out.println("</head>");
         out.println("<body>");
-        if(Objects.equals(request.getParameter("action"), "getEmployee")) {
+
+        if(Objects.equals(request.getParameter("action"), "getEmployee")) { // check action did client request
             out.println("<h1>Here are the employee data</h1>");
             out.println("<table>");
 
@@ -37,18 +39,23 @@ public class EmployeesServlet extends HttpServlet {
 
             out.println("</tr>");
             try {
-                Connection conn = DBConnection.getInstance();
+                Connection conn = DBConnection.getInstance(); // get Connection instance from Oracle Driver
 
-                Statement stmt = conn.createStatement();
+                Statement stmt = conn.createStatement(); // create statement to execute
+
+                // execute query and return rows
                 ResultSet rs = stmt.executeQuery(
                         "select employee_id, first_name, last_name, email, job_title, department_name " +
                                 "from employees " +
                                 "join jobs using (job_id) " +
                                 "join departments using (department_id)"
                 );
+
+                // check if there is any next rows in result
                 while(rs.next()) {
                     out.println("<tr>");
 
+                    // rs.getString(index) - get column by using index column
                     out.println("<td>" + rs.getString(1) + "</td>");
                     out.println("<td>" + rs.getString(2) + " " + rs.getString(3) + "</td>");
                     out.println("<td>" + rs.getString(4) + "</td>");
